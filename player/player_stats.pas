7932dc892161ee878f317ca491b6071a4ead9f2f
Chris Hawkins <cyberfilth@Latitude-E5450>
2021-08-15 14:18:44 +0100

    Levelling up to level 11 supported

--- player/player_stats.pas
+++ player/player_stats.pas
@@ -24,7 +24,25 @@ uses
 
 procedure checkLevel;
 begin
-  if (playerLevel = 1) and (entityList[0].xpReward >= 2) then
+  if (playerLevel = 1) and (entityList[0].xpReward >= 100) then
+    showLevelUpOptions
+  else if (playerLevel = 2) and (entityList[0].xpReward >= 250) then
+    showLevelUpOptions
+  else if (playerLevel = 3) and (entityList[0].xpReward >= 450) then
+    showLevelUpOptions
+  else if (playerLevel = 4) and (entityList[0].xpReward >= 700) then
+    showLevelUpOptions
+  else if (playerLevel = 5) and (entityList[0].xpReward >= 1000) then
+    showLevelUpOptions
+  else if (playerLevel = 6) and (entityList[0].xpReward >= 1350) then
+    showLevelUpOptions
+  else if (playerLevel = 7) and (entityList[0].xpReward >= 1750) then
+    showLevelUpOptions
+  else if (playerLevel = 8) and (entityList[0].xpReward >= 2200) then
+    showLevelUpOptions
+  else if (playerLevel = 9) and (entityList[0].xpReward >= 2700) then
+    showLevelUpOptions
+  else if (playerLevel = 10) and (entityList[0].xpReward >= 3250) then
     showLevelUpOptions;
 end;
 
