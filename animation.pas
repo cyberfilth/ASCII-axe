93165e77f6913c0955d660fc5a8684fd12305810
Chris Hawkins <cyberfilth@Latitude-E5450>
2021-09-08 17:25:22 +0100

    Animated rock throwing

--- animation.pas
+++ animation.pas
@@ -7,7 +7,7 @@ unit animation;
 interface
 
 uses
-  SysUtils, Classes, video, ui, entities, logging;
+  SysUtils, Classes, video, ui, entities, map;
 
 type
   a = array[1..10] of TPoint;
@@ -21,13 +21,17 @@ procedure throwRock(id: smallint; var flightPath: a);
 var
   i: byte;
 begin
-  logAction('Throw path');
   ui.displayMessage(entityList[id].race + ' throws a rock at you');
   for i := 2 to 10 do
   begin
     LockScreenUpdate;
     if (flightPath[i].X <> 0) and (flightPath[i].Y <> 0) then
     begin
+      (* Paint over previous rock *)
+      if (i > 2) then
+        TextOut(flightPath[i - 1].X, flightPath[i - 1].Y, 'lightGrey', '.');
+
+      (* Draw rock *)
       TextOut(flightPath[i].X, flightPath[i].Y, 'white', '*');
       sleep(125);
     end;
