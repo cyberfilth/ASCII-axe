3a6e5ef493d96854be15a56736cbcf4c08bed403
CyberFilth <ch22011978@gmail.com>
2021-09-15 13:38:39 +0100

    Updated version number to 34-alpha

--- globalutils.pas
+++ globalutils.pas
@@ -13,7 +13,7 @@ type
 
 const
   (* Version info - a = Alpha, d = Debug, r = Release *)
-  VERSION = '33a';
+  VERSION = '34a';
   (* Columns of the game map *)
   MAXCOLUMNS = 57;
   (* Rows of the game map *)
